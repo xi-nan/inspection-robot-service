d369ed49131965c42fc65aac37b66a7a9705e6db
黄鹏辉 <xinan.hph@gmail.com>
2020-11-24 21:02:26 +0800

    重新视频播放接口

--- commons/localfile/localfile.biz/src/main/java/com/boot/commons/localfile/controller/LocalFileDownloadController.java
+++ commons/localfile/localfile.biz/src/main/java/com/boot/commons/localfile/controller/LocalFileDownloadController.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * LocalFileController
@@ -30,10 +31,6 @@ import java.net.URLEncoder;
 public class LocalFileDownloadController {
 
     @Autowired
-    HttpServletRequest request;
-    @Autowired
-    HttpServletResponse response;
-    @Autowired
     private LocalFileService service;
 
     @NotResponseBody
@@ -46,7 +43,7 @@ public class LocalFileDownloadController {
     @NotResponseBody
     @ApiOperation("普通文件下载")
     @GetMapping(value = "/{fileId}/public")
-    public void download(@PathVariable(value = "fileId") Long fileId) throws UnsupportedEncodingException {
+    public void download(@PathVariable(value = "fileId") Long fileId, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
         File file = service.loadFile(fileId);
         // 配置文件下载
         response.setHeader("content-type", "application/octet-stream");
@@ -69,102 +66,108 @@ public class LocalFileDownloadController {
         }
     }
 
-    @NotResponseBody
     @ApiOperation("视频播放，支持断点续传拖动播放")
     @GetMapping(value = "/video/{fileId}/public")
-    public void player2(@PathVariable(value = "fileId") Long fileId) {
-        File file = service.loadFile(fileId);
-
-        response.setContentType("application/octet-stream");
-        String fileName = file.getName();
-        // 设置文件名
-        response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
-
-        long p = 0L;
-        long toLength = 0L;
-        long contentLength = 0L;
-        // 0,从头开始的全文下载；1,从某字节开始的下载（bytes=27000-）；2,从某字节开始到某字节结束的下载（bytes=27000-39000）
-        int rangeSwitch = 0;
-        long fileLength;
-        String rangBytes = "";
-        fileLength = file.length();
-
-        // tell the client to allow accept-ranges
-        response.reset();
-        response.setHeader("Accept-Ranges", "bytes");
-
-        // client requests a file block download start byte
+    public void download2(@PathVariable Long fileId, HttpServletRequest request, HttpServletResponse response) {
         String range = request.getHeader("Range");
-        if (range != null && range.trim().length() > 0 && !"null".equals(range)) {
-            response.setStatus(javax.servlet.http.HttpServletResponse.SC_PARTIAL_CONTENT);
-            rangBytes = range.replaceAll("bytes=", "");
-            if (rangBytes.endsWith("-")) {
-                // bytes=270000-
-                rangeSwitch = 1;
-                p = Long.parseLong(rangBytes.substring(0, rangBytes.indexOf("-")));
-                // 客户端请求的是270000之后的字节（包括bytes下标索引为270000的字节）
-                contentLength = fileLength - p;
-            } else {
-                // bytes=270000-320000
-                rangeSwitch = 2;
-                String temp1 = rangBytes.substring(0, rangBytes.indexOf("-"));
-                String temp2 = rangBytes.substring(rangBytes.indexOf("-") + 1);
-                p = Long.parseLong(temp1);
-                toLength = Long.parseLong(temp2);
-                // 客户端请求的是 270000-320000 之间的字节
-                contentLength = toLength - p + 1;
-            }
-        } else {
-            contentLength = fileLength;
-        }
-
-        // 如果设设置了Content-Length，则客户端会自动进行多线程下载。如果不希望支持多线程，则不要设置这个参数。
-        // Content-Length: [文件的总大小] - [客户端请求的下载的文件块的开始字节]
-        // response.setHeader("Content-Length", Long.toString(contentLength));
-
-        try (FileInputStream fis = new FileInputStream(file);
-             BufferedInputStream bis = new BufferedInputStream(fis);
-             OutputStream os = response.getOutputStream()) {
-            // 断点开始
-            // 响应的格式是:
-            // Content-Range: bytes [文件块的开始字节]-[文件的总大小 - 1]/[文件的总大小]
-            if (rangeSwitch == 1) {
-                String contentRange = "bytes " + p + "-" + (fileLength - 1) + "/" + fileLength;
-                response.setHeader("Content-Range", contentRange);
-                bis.skip(p);
-            } else if (rangeSwitch == 2) {
-                String contentRange = range.replace("=", " ") + "/" + fileLength;
-                response.setHeader("Content-Range", contentRange);
-                bis.skip(p);
-            } else {
-                String contentRange = "bytes " + "0-" + (fileLength - 1) + "/" + fileLength;
-                response.setHeader("Content-Range", contentRange);
-            }
-
-            int n;
-            long readLength = 0;
-            int bsize = 1024;
-            byte[] bytes = new byte[bsize];
-            if (rangeSwitch == 2) {
-                // 针对 bytes=27000-39000 的请求，从27000开始写数据
-                while (readLength <= contentLength - bsize) {
-                    n = bis.read(bytes);
-                    readLength += n;
-                    os.write(bytes, 0, n);
+        // StaticLog.info("current request rang:" + range);
+        File file = service.loadFile(fileId);
+        //开始下载位置
+        long startByte = 0;
+        //结束下载位置
+        long endByte = file.length() - 1;
+        // StaticLog.debug("文件开始位置：{}，文件结束位置：{}，文件总长度：{}", startByte, endByte, file.length());
+        if (range != null && range.contains("bytes=") && range.contains("-")) {
+            range = range.substring(range.lastIndexOf("=") + 1).trim();
+            String[] ranges = range.split("-");
+            try {
+                //判断range的类型
+                if (ranges.length == 1) {
+                    //类型一：bytes=-2343
+                    if (range.startsWith("-")) {
+                        endByte = Long.parseLong(ranges[0]);
+                    }
+                    //类型二：bytes=2343-
+                    else if (range.endsWith("-")) {
+                        startByte = Long.parseLong(ranges[0]);
+                    }
+                } else if (ranges.length == 2) {
+                    //类型三：bytes=22-2343
+                    startByte = Long.parseLong(ranges[0]);
+                    endByte = Long.parseLong(ranges[1]);
                 }
-                if (readLength <= contentLength) {
-                    n = bis.read(bytes, 0, (int) (contentLength - readLength));
-                    os.write(bytes, 0, n);
-                }
-            } else {
-                while ((n = bis.read(bytes)) != -1) {
-                    os.write(bytes, 0, n);
-                }
+            } catch (NumberFormatException e) {
+                startByte = 0;
+                endByte = file.length() - 1;
+                // StaticLog.error("Range Occur Error, Message:{}", e.getLocalizedMessage());
             }
-            os.flush();
-        } catch (Exception e) {
-            LocalFileErrCodeEnum.E_29503.throwThis(e.toString());
+            //要下载的长度
+            long contentLength = endByte - startByte + 1;
+            //文件名
+            String fileName = file.getName();
+            //文件类型
+            String contentType = request.getServletContext().getMimeType(fileName);
+            // 解决下载文件时文件名乱码问题
+            byte[] fileNameBytes = fileName.getBytes(StandardCharsets.UTF_8);
+            fileName = new String(fileNameBytes, 0, fileNameBytes.length, StandardCharsets.ISO_8859_1);
+            //各种响应头设置
+            //支持断点续传，获取部分字节内容：
+            response.setHeader("Accept-Ranges", "bytes");
+            //http状态码要为206：表示获取部分内容
+            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+            response.setContentType(contentType);
+            response.setHeader("Content-Type", contentType);
+            //inline表示浏览器直接使用，attachment表示下载，fileName表示下载的文件名
+            response.setHeader("Content-Disposition", "inline;filename=" + fileName);
+            response.setHeader("Content-Length", String.valueOf(contentLength));
+            // Content-Range，格式为：[要下载的开始位置]-[结束位置]/[文件总大小]
+            response.setHeader("Content-Range", "bytes " + startByte + "-" + endByte + "/" + file.length());
+
+            BufferedOutputStream outputStream = null;
+            RandomAccessFile randomAccessFile = null;
+            //已传送数据大小
+            long transmitted = 0;
+            try {
+                randomAccessFile = new RandomAccessFile(file, "r");
+                outputStream = new BufferedOutputStream(response.getOutputStream());
+                byte[] buff = new byte[4096];
+                int len = 0;
+                randomAccessFile.seek(startByte);
+                //坑爹地方四：判断是否到了最后不足4096（buff的length）个byte这个逻辑（(transmitted + len) <= contentLength）要放前面！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+                //不然会会先读取randomAccessFile，造成后面读取位置出错，找了一天才发现问题所在
+                //此处的原作者意思逻辑就是  (len = randomAccessFile.read(buff)) 每次读取4096个字节 eg 文件剩余2000 读4096 意味着 有2096
+                //是空的  那么前端解析的时候就会出错  所以此处作者加了(transmitted + len) <= contentLength
+                //条件判断
+                while ((transmitted + len) <= contentLength && (len = randomAccessFile.read(buff)) != -1) {
+                    outputStream.write(buff, 0, len);
+                    transmitted += len;
+                }
+                //处理不足buff.length部分
+                if (transmitted < contentLength) {
+                    len = randomAccessFile.read(buff, 0, (int) (contentLength - transmitted));
+                    outputStream.write(buff, 0, len);
+                    transmitted += len;
+                }
+
+                outputStream.flush();
+                response.flushBuffer();
+                randomAccessFile.close();
+                // StaticLog.debug("下载完毕：" + startByte + "-" + endByte + "：" + transmitted);
+                //捕获此异常表示拥护停止下载
+            } catch (IOException e) {
+                // e.printStackTrace();
+                // StaticLog.debug("用户停止下载：" + startByte + "-" + endByte + "：" + transmitted);
+            } finally {
+                try {
+                    if (randomAccessFile != null) {
+                        randomAccessFile.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }///end try
         }
     }
+
 
 }
